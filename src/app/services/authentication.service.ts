f3f7864993268a936ab156e940b42dbe4a540f9d
Valentin Buisson (VBU) <valentin.buisson@ntsretail.com>
2021-02-07 10:49:11 +0100

    manage user token in localstorage

--- src/app/services/authentication.service.ts
+++ src/app/services/authentication.service.ts
@@ -10,19 +10,25 @@ import {map, tap} from 'rxjs/operators';
 export class AuthenticationService {
 
   private userToken: string;
+  private readonly USER_TOKEN = 'userToken';
 
   constructor(private loginApiService: LoginApiService) {
+    this.userToken = localStorage.getItem(this.USER_TOKEN);
   }
 
   // Tries to find the userToken for the given userName and password
   // return of(true) if there is a matching user and of(false) otherwise
   public connect(userName: string, password: string): Observable<boolean> {
     return this.loginApiService.getLogin(userName, password).pipe(
-      tap((logins: LoginModel[]) => this.userToken = logins?.[0]?.token),
+      tap((logins: LoginModel[]) => {
+        this.userToken = logins?.[0]?.token;
+        localStorage.setItem(this.USER_TOKEN, this.userToken);
+      }),
       map((logins: LoginModel[]) => logins?.length > 0));
   }
 
   public logout(): void {
+    localStorage.removeItem(this.userToken);
     this.userToken = undefined;
   }
 
